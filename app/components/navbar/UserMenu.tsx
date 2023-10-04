d1ddbcfdf87ef79fe0fadd4ef5fc41c56c12b159
Op-Pautu <pautuop@gmail.com>
2023-10-04 22:16:50 +0530

    Add UI for UserMenu and Avatar

---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import Avatar from "../Avatar";
+
+const UserMenu = () => {
+  return (
+    <div className="relative">
+      <div className="flex items-center gap-3">
+        <div
+          onClick={() => {}}
+          className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
+        >
+          Airbnb your home
+        </div>
+        <div
+          onClick={() => {}}
+          className="p-4 md:px-2 md:py-1 border-[1px] border-neutral-200 flex items-center gap-3 rounded-full cursor-pointer hover:shadow-md"
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserMenu;
