96d94ae38eecfada40ba6b3d7ae77e4896426aaa
Op-Pautu <pautuop@gmail.com>
2023-10-05 23:54:49 +0530

    Finish UI for navbar categories

--- components/navbar/Categories.tsx
+++ components/navbar/Categories.tsx
@@ -17,6 +17,7 @@ import { BsSnow } from "react-icons/bs";
 import { IoDiamond } from "react-icons/io5";
 import { MdOutlineVilla } from "react-icons/md";
 import CategoryBox from "../CategoryBox";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export const categories = [
   {
@@ -97,11 +98,24 @@ export const categories = [
 ];
 
 const Categories = () => {
+  const params = useSearchParams();
+  const category = params?.get("category");
+  const pathname = usePathname();
+
+  const isMainPage = pathname === "/";
+
+  if (!isMainPage) return null;
+
   return (
     <Container>
       <div className="pt-4 flex items-center justify-between overflow-x-auto">
         {categories.map((item) => (
-          <CategoryBox key={item.label} label={item.label} icon={item.icon} />
+          <CategoryBox
+            key={item.label}
+            label={item.label}
+            icon={item.icon}
+            selected={category === item.label}
+          />
         ))}
       </div>
     </Container>
