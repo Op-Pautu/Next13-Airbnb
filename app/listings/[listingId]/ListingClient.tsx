cacc851ef24315b25225ed9c45000fa5102b4a78
Op-Pautu <pautuop@gmail.com>
2023-10-07 17:44:20 +0530

    Start building ListingInfo

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import Container from "@/components/Container";
+import ListingHead from "@/components/listings/ListingHead";
+import ListingInfo from "@/components/listings/ListingInfo";
 import { categories } from "@/components/navbar/Categories";
 import { SafeUser } from "@/types";
 import { Listing, Reservation } from "@prisma/client";
@@ -6,7 +10,9 @@ import React, { useMemo } from "react";
 
 interface ListingClientProps {
   reservations?: Reservation[];
-  listing: Listing;
+  listing: Listing & {
+    user: SafeUser;
+  };
   currentUser?: SafeUser | null;
 }
 
@@ -16,12 +22,34 @@ const ListingClient: React.FC<ListingClientProps> = ({
 }) => {
   const category = useMemo(() => {
     return categories.find((item) => item.label === listing.category);
-  
-}, [listing.category]);
+  }, [listing.category]);
 
-  return <Container>
-    <div className="max-w-screen-lg mx-auto"></div>
-  </Container>;
+  return (
+    <Container>
+      <div className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col gap-6">
+          <ListingHead
+            title={listing.title}
+            imageSrc={listing.imageSrc}
+            locationValue={listing.locationValue}
+            id={listing.id}
+            currentUser={currentUser}
+          />
+          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+            <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.bathroomCount}
+              locationValue={listing.locationValue}
+            />
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default ListingClient;
