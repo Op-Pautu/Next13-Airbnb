be4e27655020f72fb185da8c9bd68af6d6e527dd
Op-Pautu <pautuop@gmail.com>
2023-10-07 16:10:24 +0530

    Add HeartButton

--- components/listings/ListingCard.tsx
+++ components/listings/ListingCard.tsx
@@ -2,9 +2,12 @@
 
 import { SafeUser } from "@/types";
 import { Listing, Reservation } from "@prisma/client";
-import React, { useCallback } from "react";
+import React, { useCallback, useMemo } from "react";
 import { useRouter } from "next/navigation";
 import useCountries from "@/hooks/useCountries";
+import { format } from "date-fns";
+import Image from "next/image";
+import HeartButton from "../HeartButton";
 
 interface ListingCardProps {
   data: Listing;
@@ -40,7 +43,46 @@ const ListingCard: React.FC<ListingCardProps> = ({
     [onAction, actionId, disabled]
   );
 
-  return <div></div>;
+  const price = useMemo(() => {
+    if (reservation) {
+      return reservation.totalPrice;
+    }
+
+    return data.price;
+  }, [reservation, data.price]);
+
+  const reservationDate = useMemo(() => {
+    if (!reservation) return null;
+
+    const start = new Date(reservation.startDate);
+    const end = new Date(reservation.endDate);
+
+    return `${format(start, "PP")} - ${format(end, "PP")}`;
+  }, [reservation]);
+
+  return (
+    <div
+      onClick={() => router.push(`/listings/${data.id}`)}
+      className="col-span-1 cursor-pointer group"
+    >
+      <div className="flex flex-col gap-2 w-full">
+        <div className="aspect-square w-full relative overflow-hidden rounded-xl">
+          <Image
+            src={data.imageSrc}
+            alt="Listing"
+            className="object-cover w-full h-full group-hover:scale-110 transition"
+            fill
+          />
+          <div className="absolute top-3 right-3">
+            <HeartButton
+                listingId={data.id}
+                currentUser={currentUser}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ListingCard;
