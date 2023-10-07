3d4631b54715976e63857cc3b0520b6654350cec
Op-Pautu <pautuop@gmail.com>
2023-10-07 19:29:31 +0530

    Add ListingCategory and Map

--- components/listings/ListingInfo.tsx
+++ components/listings/ListingInfo.tsx
@@ -1,8 +1,16 @@
 "use client";
 
+import useCountries from "@/hooks/useCountries";
 import { SafeUser } from "@/types";
 import React from "react";
 import { IconType } from "react-icons";
+import Avatar from "../Avatar";
+import ListingCategory from "./ListingCategory";
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("../Map"), {
+  ssr: false,
+});
 
 interface ListingInfoProps {
   user: SafeUser;
@@ -29,7 +37,36 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
   category,
   locationValue,
 }) => {
-  return <div></div>;
+  const { getByValue } = useCountries();
+
+  const coordinates = getByValue(locationValue)?.latlng;
+
+  return (
+    <div className="col-span-4 flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
+        <div className="text-xl font-semibold flex items-center gap-2">
+          <div>Hosted by {user?.name}</div>
+          <Avatar src={user?.image} />
+        </div>
+        <div className="flex items-center gap-4 font-light text-neutral-500">
+          <div>{guestCount} guests</div>
+          <div>{roomCount} rooms</div>
+          <div>{bathroomCount} bathrooms</div>
+        </div>
+      </div>
+      <hr />
+      {category && (
+        <ListingCategory
+          icon={category.icon}
+          label={category.label}
+          description={category.description}
+        />
+      )}
+      <hr />
+      <div className="text-lg font-light text-neutral-500">{description}</div>
+      <Map center={coordinates} />
+    </div>
+  );
 };
 
 export default ListingInfo;
